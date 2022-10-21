54dcb6118145e5dbbb26834b34d9d83b23996019
Martin Grewe <grewe@zib.de>
2022-10-21 09:49:26 +0200

    hybrid extension/plugin for unity integration

---
+++ packages/extension-unity/index.js
@@ -0,0 +1,110 @@
+var jsPsychUnityExtension = (function (jspsych) {
+  "use strict";
+
+  /**
+   * **UNITY EXTENSION**
+   *
+   * This extension provides a way to
+   *
+   * @author YOUR NAME
+   * @see {@link https://DOCUMENTATION_URL DOCUMENTATION LINK TEXT}
+   */
+  class UnityExtension {
+    constructor(jsPsych) {
+      this.jsPsych = jsPsych;
+    }
+
+    async initialize(params) {
+      this.script = document.createElement("script");
+      this.script.src = params["loaderUrl"];
+
+      this.div = document.createElement("div");
+      this.div.id = "unity-container";
+      this.div.hidden = true;
+
+      this.canvas = document.createElement("canvas");
+      this.canvas.id = "unity-canvas";
+      this.canvas.style.width = params["width"];
+      this.canvas.style.height = params["height"];
+
+      this.div.appendChild(this.canvas);
+      document.body.appendChild(this.div);
+      document.head.appendChild(this.script);
+
+      // this.jsPsych.addFinishExperimentCallback(()=>{
+      //   this.closeUnity();
+      // });
+
+      this.myGameInstance = null;
+
+      return new Promise((resolve) => {
+        this.script.addEventListener("load", () => {
+          var config = {
+            dataUrl: params["dataUrl"],
+            frameworkUrl: params["frameworkUrl"],
+            codeUrl: params["codeUrl"],
+          };
+
+          // TODO: Indicate loading progress in jsPsych-compatible way.
+
+          createUnityInstance(this.canvas, config, (progress) => {
+            // p.innerHTML += progress;
+            console.log("Unity loading process: " + progress);
+          })
+            .catch((message) => {
+              alert(message);
+            })
+            .then((unityInstance) => {
+              this.myGameInstance = unityInstance;
+
+              if ("waitEvent" in params) {
+                const listener = () => {
+                  window.removeEventListener(params["waitEvent"], listener);
+                  resolve();
+                };
+                window.addEventListener(params["waitEvent"], listener);
+              } else {
+                console.log("No WaitEvent");
+                resolve();
+              }
+            });
+        });
+      });
+    }
+
+    addCanvasToTrial(display_element) {
+      display_element.appendChild(this.div);
+      this.div.hidden = false;
+    }
+
+    removeCanvasAfterTrial() {
+      this.div.hidden = true;
+      document.body.appendChild(this.div);
+    }
+
+    closeUnity() {
+      console.log("Experiment finished. Asking Unity to quit.");
+      this.myGameInstance.Quit();
+      this.myGameInstance = null;
+    }
+
+    sendMessage(gameObject, fct, msg) {
+      this.myGameInstance.SendMessage(gameObject, fct, msg);
+    }
+
+    on_start(params) {}
+
+    on_load(params) {}
+
+    on_finish(params) {
+      return {
+        data_property: "data_value",
+      };
+    }
+  }
+  UnityExtension.info = {
+    name: "unity",
+  };
+
+  return UnityExtension;
+})(jsPsychModule);
---
+++ packages/plugin-unity/index.js
@@ -0,0 +1,82 @@
+var jsPsychUnityPlugin = (function (jspsych) {
+  "use strict";
+
+  const info = {
+    name: "moot-plugin",
+    parameters: {
+      trial_duration: {
+        type: jspsych.ParameterType.INT,
+        default: -1,
+      },
+    },
+  };
+
+  /**
+   * **UNITY-PLUGIN**
+   *
+   * This plugin integrates unity into jsPsych
+   *
+   */
+  class UnityPlugin {
+    constructor(jsPsych) {
+      this.jsPsych = jsPsych;
+      this.unity = this.jsPsych.extensions["unity"];
+    }
+
+    trial(display_element, trial) {
+      // data saving
+      this.trial_data = {
+        unity_log: [],
+      };
+
+      window.addEventListener("JSPsychUnityEvent", this.unityEventCallback);
+
+      // end trial
+      if (trial["trial_duration"] > 0) {
+        this.jsPsych.pluginAPI.setTimeout(() => {
+          this.unity.sendMessage(
+            "Controller",
+            "OnMessage",
+            JSON.stringify({
+              channel: "phase-ended",
+              payload: {},
+            })
+          );
+          this.finish_trial();
+        }, trial["trial_duration"]);
+      }
+
+      this.unity.sendMessage(
+        "Controller",
+        "OnMessage",
+        JSON.stringify({
+          channel: "phase-started",
+          payload: { parameters: trial["unity_parameters"] },
+        })
+      );
+      this.unity.addCanvasToTrial(display_element);
+    }
+
+    finish_trial() {
+      window.removeEventListener("JSPsychUnityEvent", this.unity_event_callback);
+      this.unity.removeCanvasAfterTrial();
+      this.jsPsych.finishTrial(this.trial_data);
+    }
+
+    unityEventCallback(event) {
+      if (event.detail != undefined) {
+        try {
+          var json = JSON.parse(event.detail);
+          if (json["channel"] == "log") {
+            this.trial_data.unity_log.push(json.payload);
+          }
+        } catch (error) {
+          window.alert(error);
+        }
+      }
+    }
+  }
+  UnityPlugin.info = info;
+
+  return UnityPlugin;
+})(jsPsychModule);
